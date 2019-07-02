a661afd5dc668ba030924e23b0b2ac8e782b1bc2
wania13 <50830238+wania13@users.noreply.github.com>
2019-07-02 10:19:34 -0700

    more accurate

--- delete.php
+++ delete.php
@@ -4,7 +4,11 @@ include 'connection.php';
 
 $query2="DELETE from photos where photo_id='$_GET[photo_id]'";
 mysqli_query($conn,$query2);
- echo "deleted successfully";
+ echo "deleted successfully :)";
+
+$page = 'dash3.php';
+$sec = "1";
+header("Refresh: $sec; url=$page");
 
 
  ?>
